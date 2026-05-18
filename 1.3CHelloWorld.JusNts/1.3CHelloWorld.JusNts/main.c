7f5bbb1ecb7183a5005a2fa8122e0b3c4c9d0bc3
Oppie1 <atindall729@gmail.com>
2026-05-17 20:48:57 -0500

    Add files via upload

---
+++ 1.3CHelloWorld.JusNts/1.3CHelloWorld.JusNts/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+
+//Entry point of the program - code inside main() runs when you start the program.
+
+int main() {
+
+	//printf() prints text to the screen.
+	//The text to print goes inide the parenthese and quotes.
+	//Print "Hello World!" to the screen.
+	
+}
