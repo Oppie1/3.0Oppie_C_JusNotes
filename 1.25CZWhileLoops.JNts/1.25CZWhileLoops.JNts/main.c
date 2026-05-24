61fb8498237f76c6be1b6c91ece4a571c0a1d492
Oppie1 <atindall729@gmail.com>
2026-05-23 21:22:11 -0500

    Add files via upload

---
+++ 1.25CZWhileLoops.JNts/1.25CZWhileLoops.JNts/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//Make a program that starts out at .10 cents and doubles every day for 10 days to see how much money you would
+//get on day 10. Make an expression that takes the amount on a given day and multiplies it by 2 and then moves
+//to the increment step++ to move to the next day before going back to the top while loop that will keep running
+//so long as day<10. The program prints one line during each iteration until the 10th iteration where 
+//day = 10-> 10 < 10? False, loop stops.
+
+int main()
+{
+	//Declare an integer variable for the loop counter named day an initialize it to 1.
+	//CODE:
+
+	//Declare a float variable that stores a decimal that represents the starting value of .10 cents
+	//CODE:
+	
+	//Loop runs while day<10 (day 1-10
+	//When day reaches 10, the condition is false so the loop exits.
+	//Make a while loop that test for the day so long as it is less than 10.
+	//CODE:
+
+		//Each iteration executes these lines top-to-bottom, one at a time.
+		//Prints the current day and amount for this iteration.
+		//Example: example Day 1: =.10, Day 2 -> .20
+		//Use print function to show which day %d the loop is on and then the current value of amount on that day.
+		//CODE:
+
+		//Assign variable amount to an expression that doubles the amount. Remember you can use *= as shorthand for this step. 
+		//CODE:
+
+		//Increment the day by 1
+		//CODE:
+
+		//After day increments it goes back to the while condition and test whether the day is still less than 1.
+
+}
